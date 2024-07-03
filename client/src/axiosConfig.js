bfb3914a2924ea70808dff068354d3afdd798821
Jacob Smith <jacobskip7@gmail.com>
2024-07-03 16:36:57 -0400

    Changed a frontend env

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-if (import.meta.env.NODE_ENV === "production") {
+if (import.meta.env.MODE === "production") {
     axios.defaults.baseURL = 'https://feast-finder2-d0271ea4d475.herokuapp.com/';
 } else {
     axios.defaults.baseURL = 'http://localhost:4000';
