a02c340570526a88825fd02757b9fff89676709a
Jacob Smith <jacobskip7@gmail.com>
2024-07-12 15:08:49 -0400

    Implmented more margin fixes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         />
       </Helmet>
       <NavBar />
-      <div className="grid gap-3 mx-auto mt-20 mb-20 grid-cols-2 w-full lg:grid-cols-4 lg:w-10/12">
+      <div className="grid gap-3 mx-auto mb-20 grid-cols-2 w-full lg:grid-cols-4 lg:w-10/12">
         <h2
           style={{
             color: '#4B6D62',
