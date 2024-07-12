c6adca350415b7ad2cee79ed061b993348c3f310
Jacob Smith <jacobskip7@gmail.com>
2024-07-12 15:02:55 -0400

    Fixed margin on favorites and results page

--- client/src/components/Favorites/Favorites.jsx
+++ client/src/components/Favorites/Favorites.jsx
@@ -30,7 +30,11 @@ const Favorites = () => {
       <NavBar />
       <div className="grid gap-3 mx-auto mt-20 mb-20 grid-cols-2 w-full lg:grid-cols-4 lg:w-10/12">
         <h2
-          style={{ color: '#4B6D62', fontFamily: 'montserrat' }}
+          style={{
+            color: '#4B6D62',
+            fontFamily: 'montserrat',
+            marginTop: '136px',
+          }}
           className="font-semibold text-2xl col-span-2 lg:col-span-4"
         >
           Favorite Recipes
--- client/src/components/Results/Results.jsx
+++ client/src/components/Results/Results.jsx
@@ -20,7 +20,11 @@ const Results = () => {
       <NavBar />
       <div className="grid gap-3 mx-auto mt-20 mb-20 grid-cols-2 w-full lg:grid-cols-4 lg:w-10/12 ">
         <h2
-          style={{ color: '#4B6D62', fontFamily: 'montserrat' }}
+          style={{
+            color: '#4B6D62',
+            fontFamily: 'montserrat',
+            marginTop: '136px',
+          }}
           className="font-semibold text-2xl col-span-2 lg:col-span-4"
         >
           Search Results
